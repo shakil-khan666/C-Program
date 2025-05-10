1aeaecfd3714ac9d683ca438300f3dac81449de8
shakil-khan666 <96341854+shakil-khan666@users.noreply.github.com>
2025-05-10 22:48:58 +0600

    Add files via upload

---
+++ [2]_input.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("please enter the vaule of a: ");
+    scanf("%d",&a);
+
+    printf("please enter the vaule of : ");
+    scanf("%d",&b);
+
+    printf("A = %d\n",a);
+    printf("B = %d\n",b);
+
+    return 0;
+
+}
