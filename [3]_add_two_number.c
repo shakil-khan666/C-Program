d46e8032073ab496c7404db830b26c6d54598ea6
shakil-khan666 <96341854+shakil-khan666@users.noreply.github.com>
2025-05-11 20:02:30 +0600

    Add files via upload

---
+++ [3]_add_two_number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+int a,b,sum;
+
+    printf("please enter the vaule of a: ");
+    scanf("%d",&a);
+
+    printf("please enter the vaule of : ");
+    scanf("%d",&b);
+
+    sum = a+b;
+
+    printf("sum = %d",sum);
+
+
+return 0;
+}
