46af8c6ecf0bda67da8bfa4ffd2073c0df429058
shakil-khan666 <96341854+shakil-khan666@users.noreply.github.com>
2025-05-14 22:49:44 +0600

    Add files via upload

---
+++ [7]_upper_to_lower.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int upper;
+    printf("Enter the numbwe Lower : ");
+    scanf("%c",&upper);
+    printf("The upper case : %c",upper+32);
+
+    return 0;
+}
+
