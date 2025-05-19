411c3483d955837457010d5ac81f6eda6d60d7a5
shakil-khan666 <96341854+shakil-khan666@users.noreply.github.com>
2025-05-19 22:09:39 +0600

    Add files via upload

---
+++ [13]Area_of_Circel.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+
+    float radius,Area;
+    printf("Enter the radius  : ");
+    scanf("%f",&radius);
+    Area = 3.1416*radius*radius;
+    printf("Area = %.2f",Area);
+    return 0;
+
+}
