23c1df96b17928c5530cf8828d4248aa90dcbb30
shakil-khan666 <96341854+shakil-khan666@users.noreply.github.com>
2025-05-20 23:40:49 +0600

    Add files via upload

---
+++ [14]_Centigrade_to_Fahrenheit.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+    int C,F;
+    printf("Enter the Centigrade : ");
+    scanf("%d",&C);
+    F = (1.8*C)+32;
+
+    printf("Fahrenheit= %d : ",F);
+
+    return 0;
+}
